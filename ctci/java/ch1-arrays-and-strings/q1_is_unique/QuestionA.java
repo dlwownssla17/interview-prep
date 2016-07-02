822ee10f9ebd4cae2278c9c2f5183b64e83dea90
JJ Lee <dlwownssla17@gmail.com>
2016-07-02 19:48:56 -0400

    Some .gitignore fixes.

--- ctci/java/ch1-arrays-and-strings/q1_is_unique/QuestionA.java
+++ ctci/java/ch1-arrays-and-strings/q1_is_unique/QuestionA.java
@@ -4,4 +4,7 @@ package q1_is_unique;
  * Created by jjlee on 7/2/16.
  */
 public class QuestionA {
+    public static void main(String[] args) {
+        System.out.println("Joel is a pussy.");
+    }
 }
