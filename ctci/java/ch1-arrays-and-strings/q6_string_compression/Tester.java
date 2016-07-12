0da1d74fa0ded0f0043862d98c6401c039b75f24
JJ Lee <dlwownssla17@gmail.com>
2016-07-12 00:54:02 -0400

    Worked on ctci java & python ch1 q5.

---
+++ ctci/java/ch1-arrays-and-strings/q6_string_compression/Tester.java
@@ -0,0 +1,16 @@
+package q6_string_compression;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by jjlee on 7/12/16.
+ */
+public class Tester {
+    @Test
+    public void testCompress() throws Exception {
+
+    }
+
+}
