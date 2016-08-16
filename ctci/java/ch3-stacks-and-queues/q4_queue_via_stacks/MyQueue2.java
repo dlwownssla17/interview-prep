bd0b9a9b75253335618aaa45764ed3e7f46fed5a
JJ Lee <dlwownssla17@gmail.com>
2016-08-16 18:04:42 -0400

    Worked on ctci java ch3 q4.

---
+++ ctci/java/ch3-stacks-and-queues/q4_queue_via_stacks/MyQueue2.java
@@ -0,0 +1,43 @@
+package q4_queue_via_stacks;
+
+import java.util.NoSuchElementException;
+import java.util.Stack;
+
+/**
+ * Created by jjlee on 8/16/16.
+ */
+/* queue implemented with in and out stacks (faster for back-and-forth add and remove operations) */
+public class MyQueue2<T> {
+    private Stack<T> in;
+    private Stack<T> out;
+
+    public MyQueue2() {
+        in = new Stack<>();
+        out = new Stack<>();
+    }
+
+    public void add(T data) {
+        in.push(data);
+    }
+
+    public T remove() {
+        removeOrPeek();
+        return out.pop();
+    }
+
+    public T peek() {
+        removeOrPeek();
+        return out.peek();
+    }
+
+    private void removeOrPeek() {
+        if (in.isEmpty() && out.isEmpty()) throw new NoSuchElementException();
+        if (out.isEmpty()) transferFromInToOut();
+    }
+
+    private void transferFromInToOut() {
+        while (!in.isEmpty()) {
+            out.push(in.pop());
+        }
+    }
+}
--- ctci/java/ch3-stacks-and-queues/q4_queue_via_stacks/Tester.java
+++ ctci/java/ch3-stacks-and-queues/q4_queue_via_stacks/Tester.java
@@ -38,4 +38,33 @@ public class Tester {
         try { q.remove(); fail(); } catch(NoSuchElementException e) { } catch(Exception e) { fail(); }
         try { q.peek(); fail(); } catch(NoSuchElementException e) { } catch(Exception e) { fail(); }
     }
+
+    @Test
+    public void testMyQueue2() throws Exception {
+        MyQueue2<Integer> q = new MyQueue2<>();
+        try { q.remove(); fail(); } catch(NoSuchElementException e) { } catch(Exception e) { fail(); }
+        try { q.peek(); fail(); } catch(NoSuchElementException e) { } catch(Exception e) { fail(); }
+        q.add(0);
+        q.add(1);
+        q.add(2);
+        assertEquals((int) q.remove(), 0);
+        assertEquals((int) q.peek(), 1);
+        q.add(3);
+        q.add(4);
+        assertEquals((int) q.peek(), 1);
+        q.add(5);
+        q.add(6);
+        assertEquals((int) q.remove(), 1);
+        assertEquals((int) q.remove(), 2);
+        assertEquals((int) q.peek(), 3);
+        assertEquals((int) q.remove(), 3);
+        assertEquals((int) q.remove(), 4);
+        assertEquals((int) q.remove(), 5);
+        q.add(7);
+        assertEquals((int) q.remove(), 6);
+        assertEquals((int) q.peek(), 7);
+        assertEquals((int) q.remove(), 7);
+        try { q.remove(); fail(); } catch(NoSuchElementException e) { } catch(Exception e) { fail(); }
+        try { q.peek(); fail(); } catch(NoSuchElementException e) { } catch(Exception e) { fail(); }
+    }
 }
