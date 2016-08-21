f17fa0619147fafd9e660561e2aa09f8d0ef644f
JJ Lee <dlwownssla17@gmail.com>
2016-08-21 03:40:11 -0400

    Worked on misc java binary_tree.

---
+++ misc/java/binary_tree/BSTNode.java
@@ -0,0 +1,23 @@
+package binary_tree;
+
+/**
+ * Created by jjlee on 8/21/16.
+ */
+public class BSTNode<T> extends BinaryTreeNode<T> {
+
+    public BSTNode(T data) {
+        super(data);
+    }
+
+    public void insert(T data) {
+
+    }
+
+    public BSTNode<T> find(T data) {
+        return null;
+    }
+
+    public boolean delete(T data) {
+        return false;
+    }
+}
