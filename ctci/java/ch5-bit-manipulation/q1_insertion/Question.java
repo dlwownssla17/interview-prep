252ca58b1b9ba414750f8d1bad8528f4d45ff178
JJ Lee <dlwownssla17@gmail.com>
2016-08-22 02:18:39 -0400

    Worked on ctci java ch5 q1.

--- ctci/java/ch5-bit-manipulation/q1_insertion/Question.java
+++ ctci/java/ch5-bit-manipulation/q1_insertion/Question.java
@@ -7,7 +7,7 @@ public class Question {
     /* insert m into n from ith bit to jth bit (at least enough bits to fit in m) */
     public static int updateBits(int n, int m, int i, int j) {
         int maskRightOnes = (1 << i) - 1;
-        int maskLeftOnes = j < 31 ? ~((1 << j + 1) - 1) : 0;
+        int maskLeftOnes = j < 31 ? -1 << j + 1 : 0;
         int mask = maskRightOnes | maskLeftOnes;
         return (n & mask) | (m << i);
     }
