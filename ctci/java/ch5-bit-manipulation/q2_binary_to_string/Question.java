04a7f5a51d0fcc269bbc6664fe74f05657a38d15
JJ Lee <dlwownssla17@gmail.com>
2016-08-22 03:53:45 -0400

    Worked on ctci java ch5 q2.

--- ctci/java/ch5-bit-manipulation/q2_binary_to_string/Question.java
+++ ctci/java/ch5-bit-manipulation/q2_binary_to_string/Question.java
@@ -6,7 +6,8 @@ package q2_binary_to_string;
 public class Question {
     /* iterate over fraction bits to convert double into binary representation */
     public static String toBinaryString(double d) {
-        StringBuilder s = new StringBuilder(d > 0 ? "0." : "0");
+        if (d == 0) return "0";
+        StringBuilder s = new StringBuilder("0.");
         int i = 0;
         double frac = 0.5;
         while (d > 0 && i < 32) {
@@ -20,6 +21,17 @@ public class Question {
             i++;
         }
         if (d > 0) return "ERROR";
+        return s.toString();
+    }
+
+    /* iterate over integer bits to convert int into binary representation */
+    public static String toBinaryString(int n) {
+        if (n == 0) return "0";
+        StringBuilder s = new StringBuilder();
+        while (n != 0) {
+            s.insert(0, n & 1);
+            n >>>= 1;
+        }
         return s.toString();
     }
 }
--- ctci/java/ch5-bit-manipulation/q2_binary_to_string/Tester.java
+++ ctci/java/ch5-bit-manipulation/q2_binary_to_string/Tester.java
@@ -11,7 +11,7 @@ import static q2_binary_to_string.Question.*;
 public class Tester {
     @Test
     public void testToBinaryString() throws Exception {
-        assertEquals(toBinaryString(0), "0");
+        assertEquals(toBinaryString(0.), "0");
         assertEquals(toBinaryString(0.5), "0.1");
         assertEquals(toBinaryString(0.25), "0.01");
         assertEquals(toBinaryString(0.75), "0.11");
@@ -19,7 +19,20 @@ public class Tester {
         assertEquals(toBinaryString(1.0 / Math.pow(2, 32)), "0.00000000000000000000000000000001");
         assertEquals(toBinaryString(1.0 / 3), "ERROR");
         assertEquals(toBinaryString(0.72), "ERROR");
-        assertEquals(toBinaryString(1), "ERROR");
+        assertEquals(toBinaryString(1.), "ERROR");
+    }
+
+    @Test
+    public void testToBinaryStringInteger() throws Exception {
+        assertEquals(toBinaryString(0), "0");
+        assertEquals(toBinaryString(1), "1");
+        assertEquals(toBinaryString(2), "10");
+        assertEquals(toBinaryString(3), "11");
+        assertEquals(toBinaryString(5), "101");
+        assertEquals(toBinaryString((int) (Math.pow(2, 31) - 1)), "1111111111111111111111111111111");
+        assertEquals(toBinaryString(-1), "11111111111111111111111111111111");
+        assertEquals(toBinaryString(-2), "11111111111111111111111111111110");
+        assertEquals(toBinaryString((int) -Math.pow(2, 31)), "10000000000000000000000000000000");
     }
 
 }
