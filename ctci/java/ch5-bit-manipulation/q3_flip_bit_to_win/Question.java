c94dafdb6cd2908c7968945c70c6176c3f130cff
JJ Lee <dlwownssla17@gmail.com>
2016-08-23 02:01:12 -0400

    Worked on ctci java ch5 q3.

--- ctci/java/ch5-bit-manipulation/q3_flip_bit_to_win/Question.java
+++ ctci/java/ch5-bit-manipulation/q3_flip_bit_to_win/Question.java
@@ -4,15 +4,17 @@ package q3_flip_bit_to_win;
  * Created by jjlee on 8/22/16.
  */
 public class Question {
+    private static int SEQUENCE_LENGTH = 32;
 
     public static int longestSequence(int n) {
+        if (n == -1) return SEQUENCE_LENGTH;
         int ithBitMask = 1;
         int max = 1;
         boolean previousOne = false;
         boolean bridgeSequences = false;
         int thisOneSequence = 0;
         int lastOneSequence = 0;
-        for (int i = 0; i < 32; i++) {
+        for (int i = 0; i < SEQUENCE_LENGTH; i++) {
             boolean ithBit = (n & ithBitMask) != 0;
             if (ithBit) {
                 previousOne = true;
@@ -22,16 +24,42 @@ public class Question {
                     max = Math.max(max, thisOneSequence + 1 + (bridgeSequences ? lastOneSequence : 0));
                     lastOneSequence = thisOneSequence;
                     thisOneSequence = 0;
-                    bridgeSequences = i < 31 && (n & (ithBitMask << 1)) != 0;
+                    bridgeSequences = i < SEQUENCE_LENGTH - 1 && (n & (ithBitMask << 1)) != 0;
                     previousOne = false;
                 }
             }
             ithBitMask <<= 1;
         }
-        if (previousOne) {
-            max = Math.max(max, thisOneSequence == 32 ? thisOneSequence :
-                    thisOneSequence + 1 + (bridgeSequences ? lastOneSequence : 0));
+        return previousOne ? Math.max(max, thisOneSequence + 1 + (bridgeSequences ? lastOneSequence : 0)) : max;
+    }
+
+    public static int longestSequence2(int n) {
+        int searchingFor = 0;
+        int[] sequences = { 0, 0, 0 };
+        int max = 1;
+        for (int i = 0; i < SEQUENCE_LENGTH; i++) {
+            int thisBit = n & 1;
+            if (thisBit != searchingFor) {
+                if (searchingFor == 1) max = Math.max(max, getMaxSequence(sequences));
+                searchingFor = thisBit;
+                shift(sequences);
+            }
+            sequences[0]++;
+            n >>>= 1;
         }
-        return max;
+        if (searchingFor == 0) shift(sequences);
+        return Math.max(max, getMaxSequence(sequences));
+    }
+
+    private static int getMaxSequence(int[] sequences) {
+        if (sequences[1] == 1) return sequences[0] + 1 + sequences[2];
+        else if (sequences[1] == 0) return Math.max(sequences[0], sequences[2]);
+        else return Math.max(sequences[0], sequences[2]) + 1;
+    }
+
+    private static void shift(int[] sequences) {
+        sequences[2] = sequences[1];
+        sequences[1] = sequences[0];
+        sequences[0] = 0;
     }
 }
--- ctci/java/ch5-bit-manipulation/q3_flip_bit_to_win/Tester.java
+++ ctci/java/ch5-bit-manipulation/q3_flip_bit_to_win/Tester.java
@@ -27,4 +27,22 @@ public class Tester {
         assertEquals(longestSequence(Integer.parseUnsignedInt("11111000011100000111000111100001", 2)), 6);
     }
 
+    @Test
+    public void testLongestSequence2() throws Exception {
+        assertEquals(longestSequence2(Integer.parseUnsignedInt("00000000000000000000000000000000", 2)), 1);
+        assertEquals(longestSequence2(Integer.parseUnsignedInt("00000000000000000000000000000001", 2)), 2);
+        assertEquals(longestSequence2(Integer.parseUnsignedInt("10000000000000000000000000000000", 2)), 2);
+        assertEquals(longestSequence2(Integer.parseUnsignedInt("00000001000000000000000000000000", 2)), 2);
+        assertEquals(longestSequence2(Integer.parseUnsignedInt("00000001000000010000000100000001", 2)), 2);
+        assertEquals(longestSequence2(Integer.parseUnsignedInt("11111111111111111111111111111111", 2)), 32);
+        assertEquals(longestSequence2(Integer.parseUnsignedInt("00000000001111111000000000000111", 2)), 8);
+        assertEquals(longestSequence2(Integer.parseUnsignedInt("11111111100000000001111000000000", 2)), 10);
+        assertEquals(longestSequence2(Integer.parseUnsignedInt("01111111111111111111111111111111", 2)), 32);
+        assertEquals(longestSequence2(Integer.parseUnsignedInt("11111111111111101111111111111111", 2)), 32);
+        assertEquals(longestSequence2(Integer.parseUnsignedInt("11111111111111101111111110111111", 2)), 25);
+        assertEquals(longestSequence2(Integer.parseUnsignedInt("11011111001111110000001110100011", 2)), 8);
+        assertEquals(longestSequence2(Integer.parseUnsignedInt("11111000011101110111000111110000", 2)), 7);
+        assertEquals(longestSequence2(Integer.parseUnsignedInt("11111000011100000111000111100001", 2)), 6);
+    }
+
 }
