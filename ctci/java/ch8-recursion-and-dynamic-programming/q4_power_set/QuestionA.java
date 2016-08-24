351aaf02513ce379785aaea89b58df11e24d0124
JJ Lee <dlwownssla17@gmail.com>
2016-08-23 22:50:29 -0400

    Worked on ctci java ch8 q4.

--- ctci/java/ch8-recursion-and-dynamic-programming/q4_power_set/QuestionA.java
+++ ctci/java/ch8-recursion-and-dynamic-programming/q4_power_set/QuestionA.java
@@ -1,7 +1,34 @@
 package q4_power_set;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 /**
  * Created by jjlee on 8/23/16.
  */
 public class QuestionA {
+
+    public static <T> ArrayList<ArrayList<T>> powerSet(ArrayList<T> set) {
+        if (set.isEmpty()) return new ArrayList<>();
+        return powerSetHelper(set);
+    }
+
+    private static <T> ArrayList<ArrayList<T>> powerSetHelper(ArrayList<T> set) {
+        if (set.isEmpty()) {
+            ArrayList<ArrayList<T>> baseSet = new ArrayList<>();
+            baseSet.add(new ArrayList<>());
+            return baseSet;
+        }
+        T item = set.remove(set.size() - 1);
+        ArrayList<ArrayList<T>> powerSet = powerSetHelper(set);
+        ArrayList<ArrayList<T>> powerSetWithThisItem = new ArrayList<>();
+        for (ArrayList<T> subset : powerSet) {
+            ArrayList<T> newSubset = new ArrayList<>();
+            newSubset.addAll(subset);
+            newSubset.add(item);
+            powerSetWithThisItem.add(newSubset);
+        }
+        powerSet.addAll(powerSetWithThisItem);
+        return powerSet;
+    }
 }
---
+++ ctci/java/ch8-recursion-and-dynamic-programming/q4_power_set/Tester.java
@@ -0,0 +1,75 @@
+package q4_power_set;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+
+import static org.testng.Assert.*;
+import static q4_power_set.QuestionA.*;
+import static q4_power_set.QuestionB.*;
+
+/**
+ * Created by jjlee on 8/23/16.
+ */
+public class Tester {
+    ArrayList<Integer> a, b, c, d, e;
+
+    @BeforeMethod
+    public void setup() throws Exception {
+        a = new ArrayList<>();
+        b = new ArrayList<>();
+        c = new ArrayList<>();
+        d = new ArrayList<>();
+        e = new ArrayList<>();
+        b.add(0);
+        c.add(0);
+        c.add(1);
+        d.add(-5);
+        d.add(-1);
+        d.add(3);
+        d.add(7);
+        e.add(7);
+        e.add(-1);
+        e.add(3);
+        e.add(-5);
+    }
+
+    @Test
+    public void testPowerSet() throws Exception {
+        ArrayList<ArrayList<Integer>> powerSetA = powerSet(a);
+        assertTrue(powerSetA.isEmpty());
+        printPowerSet(powerSetA);
+        System.out.println("---");
+
+        ArrayList<ArrayList<Integer>> powerSetB = powerSet(b);
+        assertEquals(powerSetB.size(), 2);
+        printPowerSet(powerSetB);
+        System.out.println("---");
+
+        ArrayList<ArrayList<Integer>> powerSetC = powerSet(c);
+        assertEquals(powerSetC.size(), 4);
+        printPowerSet(powerSetC);
+        System.out.println("---");
+
+        ArrayList<ArrayList<Integer>> powerSetD = powerSet(d);
+        assertEquals(powerSetD.size(), 16);
+        printPowerSet(powerSetD);
+        System.out.println("---");
+
+        ArrayList<ArrayList<Integer>> powerSetE = powerSet(e);
+        assertEquals(powerSetE.size(), 16);
+        printPowerSet(powerSetE);
+        System.out.println("---");
+    }
+
+    private static <T> void printPowerSet(ArrayList<ArrayList<T>> powerSet) {
+        for (ArrayList<T> subset : powerSet) {
+            for (T item : subset) {
+                System.out.print(item.toString() + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
