9ffec005908e5e81fda25667e8d374c4f9cca582
JJ Lee <dlwownssla17@gmail.com>
2016-08-23 23:28:16 -0400

    Worked on ctci java ch8 q4.

--- ctci/java/ch8-recursion-and-dynamic-programming/q4_power_set/QuestionB.java
+++ ctci/java/ch8-recursion-and-dynamic-programming/q4_power_set/QuestionB.java
@@ -27,4 +27,25 @@ public class QuestionB {
         computeSubsets(set, powerSet, index + 1, buf);
         buf.remove(buf.size() - 1);
     }
+
+    /* compute all subsets of given set using combinatorics with binary representation of integers (similar to above) */
+    public static <T> ArrayList<ArrayList<T>> powerSetCombinatorics(ArrayList<T> set) {
+        if (set.isEmpty()) return new ArrayList<>();
+        int max = 1 << set.size();
+        ArrayList<ArrayList<T>> powerSet = new ArrayList<>();
+        for (int i = 0; i < max; i++) {
+            powerSet.add(convertIntToSubset(set, i));
+        }
+        return powerSet;
+    }
+
+    private static <T> ArrayList<T> convertIntToSubset(ArrayList<T> set, int x) {
+        int index = 0;
+        ArrayList<T> subset = new ArrayList<>();
+        for (int i = x; i > 0; i >>>= 1) {
+            if ((i & 1) == 1) subset.add(set.get(index));
+            index++;
+        }
+        return subset;
+    }
 }
--- ctci/java/ch8-recursion-and-dynamic-programming/q4_power_set/Tester.java
+++ ctci/java/ch8-recursion-and-dynamic-programming/q4_power_set/Tester.java
@@ -91,6 +91,34 @@ public class Tester {
         System.out.println("---");
     }
 
+    @Test
+    public void testPowerSetCombinatorics() throws Exception {
+        ArrayList<ArrayList<Integer>> powerSetA = powerSetCombinatorics(a);
+        assertTrue(powerSetA.isEmpty());
+        printPowerSet(powerSetA);
+        System.out.println("---");
+
+        ArrayList<ArrayList<Integer>> powerSetB = powerSetCombinatorics(b);
+        assertEquals(powerSetB.size(), 2);
+        printPowerSet(powerSetB);
+        System.out.println("---");
+
+        ArrayList<ArrayList<Integer>> powerSetC = powerSetCombinatorics(c);
+        assertEquals(powerSetC.size(), 4);
+        printPowerSet(powerSetC);
+        System.out.println("---");
+
+        ArrayList<ArrayList<Integer>> powerSetD = powerSetCombinatorics(d);
+        assertEquals(powerSetD.size(), 16);
+        printPowerSet(powerSetD);
+        System.out.println("---");
+
+        ArrayList<ArrayList<Integer>> powerSetE = powerSetCombinatorics(e);
+        assertEquals(powerSetE.size(), 16);
+        printPowerSet(powerSetE);
+        System.out.println("---");
+    }
+
     private static <T> void printPowerSet(ArrayList<ArrayList<T>> powerSet) {
         for (ArrayList<T> subset : powerSet) {
             for (T item : subset) {
