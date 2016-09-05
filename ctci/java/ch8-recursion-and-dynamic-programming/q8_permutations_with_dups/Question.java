08d108bb9d924163d297dde609d7e1f732c6cdd7
JJ Lee <dlwownssla17@gmail.com>
2016-09-05 18:49:44 -0400

    Worked on ctci java ch8 q8.

--- ctci/java/ch8-recursion-and-dynamic-programming/q8_permutations_with_dups/Question.java
+++ ctci/java/ch8-recursion-and-dynamic-programming/q8_permutations_with_dups/Question.java
@@ -1,7 +1,50 @@
 package q8_permutations_with_dups;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Created by jjlee on 9/5/16.
  */
 public class Question {
+    /* compute all permutations of a string with duplicate characters by keeping track of the counts of each character */
+    public static ArrayList<String> permutationsWithDups(String s) {
+        HashMap<Character, Integer> counts = buildCounts(s);
+        ArrayList<String> permutations = new ArrayList<>();
+        permutationsWithDupsHelper(permutations, counts, s.length(), new ArrayList<>());
+        return permutations;
+    }
+
+    private static HashMap<Character, Integer> buildCounts(String s) {
+        HashMap<Character, Integer> counts = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!counts.containsKey(c)) counts.put(c, 0);
+            counts.put(c, counts.get(c) + 1);
+        }
+        return counts;
+    }
+
+    private static void permutationsWithDupsHelper(ArrayList<String> permutations,
+                                                                HashMap<Character, Integer> counts, int remaining,
+                                                                ArrayList<Character> buf) {
+        if (remaining == 0) {
+            StringBuilder sb = new StringBuilder();
+            for (char c : buf) {
+                sb.append(c);
+            }
+            permutations.add(sb.toString());
+            return;
+        }
+        for (char c : counts.keySet()) {
+            int count = counts.get(c);
+            if (count > 0) {
+                counts.put(c, count - 1);
+                buf.add(c);
+                permutationsWithDupsHelper(permutations, counts, remaining - 1, buf);
+                buf.remove(buf.size() - 1);
+                counts.put(c, count);
+            }
+        }
+    }
 }
--- ctci/java/ch8-recursion-and-dynamic-programming/q8_permutations_with_dups/Tester.java
+++ ctci/java/ch8-recursion-and-dynamic-programming/q8_permutations_with_dups/Tester.java
@@ -1,16 +1,86 @@
 package q8_permutations_with_dups;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
 import static org.testng.Assert.*;
+import static q8_permutations_with_dups.Question.*;
 
 /**
  * Created by jjlee on 9/5/16.
  */
 public class Tester {
+    HashSet<String> permutationsCat, permutationsFive, permutationsEevee, permutationsWww;
+
+    @BeforeMethod
+    public void setup() throws Exception {
+        permutationsCat = new HashSet<>();
+        permutationsCat.add("cat");
+        permutationsCat.add("cta");
+        permutationsCat.add("act");
+        permutationsCat.add("atc");
+        permutationsCat.add("tca");
+        permutationsCat.add("tac");
+        permutationsFive = new HashSet<>();
+        permutationsFive.add("five");
+        permutationsFive.add("fiev");
+        permutationsFive.add("fvie");
+        permutationsFive.add("fvei");
+        permutationsFive.add("feiv");
+        permutationsFive.add("fevi");
+        permutationsFive.add("ifve");
+        permutationsFive.add("ifev");
+        permutationsFive.add("ivfe");
+        permutationsFive.add("ivef");
+        permutationsFive.add("iefv");
+        permutationsFive.add("ievf");
+        permutationsFive.add("vfie");
+        permutationsFive.add("vfei");
+        permutationsFive.add("vife");
+        permutationsFive.add("vief");
+        permutationsFive.add("vefi");
+        permutationsFive.add("veif");
+        permutationsFive.add("efiv");
+        permutationsFive.add("efvi");
+        permutationsFive.add("eifv");
+        permutationsFive.add("eivf");
+        permutationsFive.add("evfi");
+        permutationsFive.add("evif");
+        permutationsEevee = new HashSet<>();
+        permutationsEevee.add("eeeev");
+        permutationsEevee.add("eeeve");
+        permutationsEevee.add("eevee");
+        permutationsEevee.add("eveee");
+        permutationsEevee.add("veeee");
+        permutationsWww = new HashSet<>();
+        permutationsWww.add("www");
+    }
+
     @Test
     public void testPermutationsWithDups() throws Exception {
-
+        ArrayList<String> cat = permutationsWithDups("cat");
+        assertEquals(cat.size(), 6);
+        for (String permutation : cat) {
+            assertTrue(permutationsCat.contains(permutation));
+        }
+        ArrayList<String> five = permutationsWithDups("five");
+        assertEquals(five.size(), 24);
+        for (String permutation : five) {
+            assertTrue(permutationsFive.contains(permutation));
+        }
+        ArrayList<String> eevee = permutationsWithDups("eevee");
+        assertEquals(eevee.size(), 5);
+        for (String permutation : eevee) {
+            assertTrue(permutationsEevee.contains(permutation));
+        }
+        ArrayList<String> www = permutationsWithDups("www");
+        assertEquals(www.size(), 1);
+        for (String permutation : www) {
+            assertTrue(permutationsWww.contains(permutation));
+        }
     }
 
 }
