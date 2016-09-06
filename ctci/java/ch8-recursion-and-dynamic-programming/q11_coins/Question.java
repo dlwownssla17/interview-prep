a58611e187a609b2f9e2c8a3f0b4a715e142fe13
JJ Lee <dlwownssla17@gmail.com>
2016-09-06 03:07:37 -0400

    Worked on ctci java ch8 q11.

--- ctci/java/ch8-recursion-and-dynamic-programming/q11_coins/Question.java
+++ ctci/java/ch8-recursion-and-dynamic-programming/q11_coins/Question.java
@@ -8,19 +8,18 @@ public class Question {
     public static int makeChange(int n) {
         if (n < 0) return 0;
         int[] changes = { 25, 10, 5, 1 };
-        return makeChange(n, changes, 0, new int[n]);
+        return makeChange(n, changes, 0, new int[n + 1][changes.length]);
     }
 
-    private static int makeChange(int n, int[] changes, int index, int[] memo){
-        if (n < 0) return 0;
-        if (n == 0 || changes[index] == 1) return 1;
-        if (memo[n - 1] == 0) {
+    private static int makeChange(int n, int[] changes, int index, int[][] memo){
+        if (index >= changes.length - 1) return 1;
+        if (memo[n][index] == 0) {
             int sum = 0;
             for (int i = 0; i <= n / changes[index]; i++) {
                 sum += makeChange(n - i * changes[index], changes, index + 1, memo);
             }
-            memo[n - 1] = sum;
+            memo[n][index] = sum;
         }
-        return memo[n - 1];
+        return memo[n][index];
     }
 }
